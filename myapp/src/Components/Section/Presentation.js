2c50907cd49412e8cf787096767e1a7c15915f64
fernandesygor <ramos.ygor.123@gmail.com>
2023-08-29 15:34:46 -0300

    feat: Vs 0.1

--- myapp/src/Components/Section/Presentation.js
+++ myapp/src/Components/Section/Presentation.js
@@ -1,30 +1,21 @@
+import styles from './Presentation.module.css'
+import ButtonA from '../elements/ButtonA'
+
+
 function Presentation(){
     return(
-        <div id="Presentation">
-            <h1>Aqui ficara meu Footer</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br><br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+        <div className={styles.Presentation} id="Presentation">
+            <p><strong>Bem-vindo ao meu Portfólio</strong></p>
+            <h1>Olá, eu sou Ygor Fernades!</h1>
+            <p> 
+                Sou um apaixonado por tecnologia e soluções inovadoras.
+                Como Product Manager, eu tenho o compromisso de resolver problemas
+                complexos e trazer resultados excepcionais para os negócios.
+                Meus projetos já geraram milhões de reais em receita anual
+                estou sempre em busca de novos desafios para superar.
+            </p>
+            <ButtonA link='https://www.linkedin.com/in/ygor-ramos-57a648184/'  text='Conecte-se comigo!'/>
+            
         </div>
     )
 
--- myapp/src/Components/Section/Skills.js
+++ myapp/src/Components/Section/Skills.js
@@ -1,7 +1,27 @@
-function Skills(){
+import styles from './Skills.module.css'
+import javascript from '../../Image/skills/javascript.svg'
+import html from '../../Image/skills/html.svg'
+import css from '../../Image/skills/css.svg'
+import typescript from '../../Image/skills/typescript.svg'
+import react from '../../Image/skills/react.svg'
+
+
+
+
+
+function Skills (){
     return(
-        <div id="Skills">
-            <h1>Aqui ficara meu Footer</h1>
+        <div className={styles.skill} id="Skills">
+            <h1>Habilidades</h1>
+            <p>Conheça um pouco das minhas principais habilidades e conhecimentos.</p>
+            <div>
+                <img src={javascript}/>
+                <img src={html}/>
+                <img src={css}/>
+                <img src={react}/>
+                <img src={typescript}/>
+
+            </div>
         </div>
     )
 
