055405aa293ca235bc4d6393cf9c7dbd28a43b22
fernandesygor <ramos.ygor.123@gmail.com>
2023-08-29 15:35:07 -0300

    Vs 0.1

--- myapp/src/Components/Section/Projects.js
+++ myapp/src/Components/Section/Projects.js
@@ -1,7 +1,43 @@
+import ButtonB from '../elements/ButtonB'
+import styles from './Projcts.module.css'
+import Card from '../elements/Card'
+import lpdnc from '../../Image/projects/lpdnc.svg'
+import lparquitetura from '../../Image/projects/lparquitetura.svg'
+import Portifolio from '../../Image/projects/Portifolio.svg'
+
+
 function Projects(){
     return(
-        <div id="Projects">
-            <h1>Aqui ficara meus Projetos</h1>
+        <div className={styles.projects} id="Projects">
+            <h1>Projetos</h1>
+            <Card 
+            img={lpdnc}
+            title="LP - DNC"
+            tech=" HTML, CSS e JS " 
+            descripton="Este é o projeto de desenvolvimento do módulo do curso de Tecnologia da escola DNC."
+            repo='https://github.com/fernandesygor/Projeto-landingpage' 
+            site='https://escoladnc.netlify.app' 
+            />
+
+            <Card 
+            img={lparquitetura}
+            title="LP - Tradição em projetos de arquitetura"
+            tech=" HTML, CSS e JS " 
+            descripton="Este é o projeto de desenvolvimento do módulo do curso de Tecnologia da escola DNC."
+            repo='https://github.com/fernandesygor/Projeto-landing-page-Arquitetura' 
+            site='https://landing-page-pag-arquitetura.netlify.app' />
+
+            <Card
+            img={Portifolio}
+            title="LP - DNC"
+            tech=" HTML, CSS e JS " 
+            descripton="Este é o projeto de desenvolvimento do módulo do curso de Tecnologia da escola DNC."
+            repo='' 
+            site='' 
+            />
+            
+
+            <ButtonB text='Acesse meu repositório' link='https://github.com/fernandesygor?tab=repositories' /> 
         </div>
     )
 
