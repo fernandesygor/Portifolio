e3510af059c0acda572ae4daa703aedb27d47d8d
fernandesygor <ramos.ygor.123@gmail.com>
2023-09-05 19:02:30 -0300

    feat: finish Vs 0.0.1

--- myapp/src/Components/Section/Presentation.js
+++ myapp/src/Components/Section/Presentation.js
@@ -4,22 +4,45 @@ import { useEffect, useState } from 'react'
 
 
 function Presentation(){
-    const [text, setText] = useState('')
-    const toRotate = ['Ygor Francisco!', 'Desenvolvedor Front End', 'Product Manager']
-    const [loop,setLoop] = useState(0)
+    const [text, setText] = useState('');
+    const toRotate = ['Ygor Francisco!', 'Desenvolvedor Front End', 'Product Manager'];
+    const [loop,setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta]= useState (100)
 
 
     useEffect(()=>{
-        let ticker = setInterval(2000) 
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta) 
         return()=> {clearInterval(ticker)}   
 
     },[text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fulltext = toRotate[i]
+        let updatedtext = isDeleting ? fulltext.substring(0,text.length-1) : fulltext.substring(0,text.length+1)
+
+        setText(updatedtext);
+
+        if(!isDeleting && updatedtext === fulltext){
+            setIsDeleting(true)
+            setDelta(period)
+        } else if (isDeleting && updatedtext === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+
+        }
+    }
 
 
     return(
         <div className={styles.Presentation} id="Presentation">
             <p><strong>Bem-vindo ao meu Portfólio</strong></p>
-            <h1>Olá, eu sou Ygor Fernades!</h1>
+            <h1>Olá, eu sou {text}!</h1>
             <p> 
                 Sou um apaixonado por tecnologia e soluções inovadoras.
                 Como Product Manager, eu tenho o compromisso de resolver problemas
